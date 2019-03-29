38c23fccb9d770d2f19d9a9d83d39397a33d5a15
stanislawkostenko <rng92@mail.ru>
2019-03-29 10:36:36 +0300

    Task 2

--- variables/main.js
+++ variables/main.js
@@ -11,3 +11,18 @@ z = 0;
 result = x * y + 5 * z + x - 1;
 
 console.log(`Answer is ${result}`);
+
+// Task 2
+
+let algebra;
+let geography;
+let physics;
+
+algebra = 5;
+geography = 3;
+physics = 4;
+
+let averageMark;
+averageMark = (algebra + geography + physics) / 3;
+
+console.log(`Average mark is ${averageMark}`);
