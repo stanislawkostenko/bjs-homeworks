56cb08cfb42c75131d83677a981cb8a30aacb132
stanislawkostenko <rng92@mail.ru>
2019-04-02 19:41:04 +0300

    Task 2 and 3

--- functions/main.js
+++ functions/main.js
@@ -43,3 +43,74 @@ function showSolutionsMessage(a, b, c) {
     console.log(`Уравнение имеет два корня. X₁ = ${x1}, X₂ = ${x2}`);
   }
 }
+
+// Задание 2
+
+let keyToShyfre1 = {
+  aaa: 0,
+  bbb: 0
+}
+let keyToShyfre2 = {
+  aaa: 0,
+  bbb: 1
+}
+let keyToShyfre3 = {
+  aaa: 1,
+  bbb: 0
+}
+let keyToShyfre4 = {
+  aaa: 1,
+  bbb: 1
+}
+
+function getPersonData(secretData) {
+
+  function getPersonName() {
+
+    secretData = keyToShyfre3;
+
+    keyToShyfre3 = {
+      firstName: 'Родриго',
+      lastName: 'Эмильо'
+    }
+  }
+}
+
+// Задание 3
+
+function getAverageScore(data) {
+
+  //data = { name: value = [] };
+
+  for (let prop in data) {
+    let value = data[prop];
+    console.log(`${prop}, ${value}`)
+  }
+
+  function countMiddleValue() {
+   let marksSummary = 0;
+   value.forEach(function(value, index) {
+   marksSummary += +value;
+   console.log(marksSummary);
+   });
+ }
+
+}
+
+
+
+// function sum(...value in data) {
+//   let total = 0;
+//   for (let i = 0; i < value.length; i++) {
+//     total += value[ i ];
+//   }
+//
+//   let midVal = sum() / value.length;
+//
+// }
+// console.log( sum( 2, 4, 5, 16, 7, 10, 11 )); // 55
+
+getAverageScore({
+  alg: [2, 3, 2],
+  fgf: [4, 5, 2, 3, 4]
+});
