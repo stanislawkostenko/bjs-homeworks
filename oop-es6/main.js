91755c755858764e585b4e1f724420187830fbc8
stanislawkostenko <rng92@mail.ru>
2019-04-13 00:14:28 +0300

    Changed title of task 1 to task 3 and fixed it

--- oop-es6/main.js
+++ oop-es6/main.js
@@ -1,9 +1,11 @@
-// Task 1
+// Task 3
 
 class StudentLog {
 
   constructor(name) {
     this.name = name;
+    this.grade = new Array();
+    //this.subject = subject;
   }
 
   getName() {
@@ -11,75 +13,54 @@ class StudentLog {
   }
 
   addGrade(grade, subject) {
-    this.grade = grade;
-    this.subject = subject;
-    if ((grade > 5) || (grade === String(grade))) {
-      return (`Ошибка! Вы пытались поставить оценку "${grade}" по предмету "${subject}". Допустимый формат оценки: 0-5.`);
+    if ((grade > 5) || (grade < 1)) {
+      console.log(`Ошибка! Вы пытались поставить оценку "${grade}" по предмету "${subject}". Допустимый формат оценки: 0-5.`);
+      return this.grade.length;
     }
-    return (`${subject}: ${grade}`);
+    if (grade === String(grade)) {
+      this.grade = [];
+      console.log(`Ошибка! Вы пытались поставить оценку "${grade}" по предмету "${subject}". Допустимый формат оценки: 0-5.`);
+      return this.grade.length;
+    }
+    this.grade.push(grade);
+    return this.grade.length;
   }
 
   getAverageBySubject(subject) {
-    if (!subject) {
+    if (this.grade.length === 0) {
       return 0;
-    } else {
-      let average = 0;
-      for (let i = 0; i < this.grade.length; i++) {
-        average += this.grade[i];
-      }
-      average = average / this.grade.length;
-      return (`${subject}: ${average}`);
     }
+    let average = 0;
+    for (let i = 0; i < this.grade.length; i++) {
+      average += this.grade[i];
+    }
+    average = average / this.grade.length;
+    return average;
   }
 
-  getTotalAverage() {
-    let marksAverage = new Object();
-    let marksSummary = 0;
-    let counter = 0;
-
-    let marksAverageTotal = marksSummary / counter;
-    marksAverage["average"] = marksAverageTotal;
-    return marksAverage;
-
-    function countAverageValue(args) {
-      let total = 0;
-      for (let i = 0; i < args.length; i++) {
-        total += args[ i ];
-      }
-      return total / args.length;
-    }
-  }
-
-  getGradesBySubject(subject) {
-    if (!subject) {
-      return this.grade = [];
-    } else {
-      this.grade = new Array();
-      return (`${subject}: ${this.grade}`)
-    }
-  }
-
-  getGrades() {
-    if (!this.subject) {
-      return this.grade = [];
-    } else {
-      let allGrades = new Object();
-      this.grade = new Array();
-      return allGrades = {
-        subject: this.grade
-      }
-    }
-  }
+  // getTotalAverage() {
+  //   let marksAverage = new Object();
+  //   let marksSummary = 0;
+  //   let counter = 0;
+  //
+  //   let marksAverageTotal = marksSummary / counter;
+  //   marksAverage["average"] = marksAverageTotal;
+  //   return marksAverage;
+  //
+  //   function countAverageValue(args) {
+  //     let total = 0;
+  //     for (let i = 0; i < args.length; i++) {
+  //       total += args[ i ];
+  //     }
+  //     return total / args.length;
+  //   }
+  // }
 
 }
 
 
 
-let log = new StudentLog('Олег Никифоров');
-log.addGrade(4, "algebra");
-log.addGrade(3, "algebra");
-log.addGrade(3, "russian");
-log.addGrade(5, "russian");
+const log = new StudentLog('Олег Никифоров');
 
 console.log(log.getName());
 
@@ -87,14 +68,17 @@ console.log(log.addGrade(4, "algebra"));
 console.log(log.addGrade(9, "algebra"));
 console.log(log.addGrade("отлично", "algebra"));
 
+log.addGrade(4, "algebra");
+log.addGrade(4, "algebra");
+log.addGrade(5, "algebra");
+log.addGrade(3, "algebra");
+log.addGrade(3, "russian");
+log.addGrade(5, "russian");
+log.addGrade(3, "russian");
+log.addGrade(3, "russian");
+
 console.log(log.getAverageBySubject('algebra'));
 console.log(log.getAverageBySubject('russian'));
 console.log(log.getAverageBySubject('english'));
 
-console.log(log.getTotalAverage());
-
-console.log(log.getGradesBySubject('algebra'));
-console.log(log.getGradesBySubject('russian'));
-console.log(log.getGradesBySubject('english'));
-
-console.log(log.getGrades());
+// console.log(log.getTotalAverage());
