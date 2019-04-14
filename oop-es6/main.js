7bd1510fde9fafbf8f10a720625237f79110fa38
stanislawkostenko <rng92@mail.ru>
2019-04-14 12:19:34 +0300

    Task 2 is fixed

--- oop-es6/main.js
+++ oop-es6/main.js
@@ -122,31 +122,131 @@ console.log(bow.getDamage());
 
 
 
-console.log(bow.durability); // 0
-console.log(bow.isBroken()); // true
+console.log(bow.durability);
+console.log(bow.isBroken());
 
-console.log(arm.durability); // Infinity
-console.log(bow.isBroken()); // false
+console.log(arm.durability);
+console.log(bow.isBroken());
 
 // Task 2
 
-class Saber extends Weapon {
+class arm1 extends Weapon {
     constructor() {
         super({
-            name: 'Сабля',
-            attack: 22,
-            durability: 40,
+            name: 'Рука',
+            attack: 1,
+            durability: Infinity,
             range: 1
           });
         }
       };
+const arm2 = new arm1();
+console.log(arm2.name, arm2.attack, arm2.durability, arm2.range);
 
-const catana = new Saber();
+class bow1 extends Weapon {
+    constructor() {
+        super({
+            name: 'Лук',
+            attack: 10,
+            durability: 200,
+            range: 3
+          });
+        }
+      };
+const bow2 = new bow1();
+console.log(bow2.name, bow2.attack, bow2.durability, bow2.range);
 
-console.log(catana.name);
-console.log(catana.attack);
-console.log(catana.durability);
-console.log(catana.range);
+class sword1 extends Weapon {
+    constructor() {
+        super({
+            name: 'Меч',
+            attack: 25,
+            durability: 500,
+            range: 1
+          });
+        }
+      };
+const sword2 = new sword1();
+console.log(sword2.name, sword2.attack, sword2.durability, sword2.range);
+
+class knife1 extends Weapon {
+    constructor() {
+        super({
+            name: 'Нож',
+            attack: 5,
+            durability: 300,
+            range: 1
+          });
+        }
+      };
+const knife2 = new knife1();
+console.log(knife2.name, knife2.attack, knife2.durability, knife2.range);
+
+class staff1 extends Weapon {
+    constructor() {
+        super({
+            name: 'Посох',
+            attack: 8,
+            durability: 300,
+            range: 2
+          });
+        }
+      };
+const staff2 = new staff1();
+console.log(staff2.name, staff2.attack, staff2.durability, staff2.range);
+
+
+
+class longBow1 extends bow1 {
+    constructor(object) {
+        super(object);
+        this.name = object.name;
+        this.parent = object.parent;
+        this.attack = object.attack;
+        this.range = object.range;
+    }
+}
+const longBow2 = new longBow1({
+    name: 'Длинный лук',
+    parent: bow.name,
+    attack: 15,
+    range: 3
+})
+console.log(longBow2.name, longBow2.parent, longBow2.attack, longBow2.range, longBow2.durability);
+
+class battleAxe1 extends sword1 {
+    constructor(object) {
+        super(object);
+        this.name = object.name;
+        this.parent = object.parent;
+        this.attack = object.attack;
+        this.durability = object.durability;
+    }
+}
+const battleAxe2 = new battleAxe1({
+    name: 'Секира',
+    parent: sword.name,
+    attack: 27,
+    durability: 800
+})
+console.log(battleAxe2.name, battleAxe2.parent, battleAxe2.attack, battleAxe2.range, battleAxe2.durability);
+
+class stormStaff1 extends staff1 {
+    constructor(object) {
+        super(object);
+        this.name = object.name;
+        this.parent = object.parent;
+        this.attack = object.attack;
+        this.range = object.range;
+    }
+}
+const stormStaff2 = new stormStaff1({
+    name: 'Посох бури',
+    parent: staff.name,
+    attack: 10,
+    range: 3
+})
+console.log(stormStaff2.name, stormStaff2.parent, stormStaff2.attack, stormStaff2.range, stormStaff2.durability);
 
 // Task 3
 
