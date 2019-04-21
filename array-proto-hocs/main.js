4b29e009034c79a4010d14bb1535ed2374f137c8
stanislawkostenko <rng92@mail.ru>
2019-04-21 23:35:59 +0300

    Made task

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -12,6 +12,30 @@ console.log(compareArrays([1, 2, 3], [1, 2, 3]));
 console.log(compareArrays([1, 2, 3], [1, 2, 2]));
 console.log(compareArrays([1, 2, 3], [1, 2, 3, 1]));
 
+const sum = (a, b) => a + b;
+const mSum = memoize(sum, 2);
+
 function memoize(fn, limit) {
-  
+    const results = new Array();
+
+    return function() {
+        const args = Array.from(arguments);
+
+        if (results.length > limit) {
+            results.pop();
+            console.log('Превышен лимит')
+        } else {
+            results.push({
+                args: args,
+                result: fn.apply(null, Array.from(arguments))
+            });
+            console.log(results);
+            return results;
+        }
+    }
 }
+
+mSum(5, 20)
+mSum(5, 20)
+mSum(5, 21)
+mSum(5, 22)
